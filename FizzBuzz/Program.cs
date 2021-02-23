307f1fb27d1b9b5bc5df878a821c23404745e276
Tom Haywood <tom.haywood@myob.com>
2021-02-23 11:48:33 +1100

    completed FizzBuzz challenge

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,11 +8,19 @@ namespace FizzBuzz
         {
             Console.WriteLine("Hello World!");
 
-            for (var i = 0; i <= 100; i++)
+            for (var i = 1; i <= 100; i++)
             {
-                Console.WriteLine(i);
-                
-}
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
