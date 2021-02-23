e3a75156a3857130c82b66b4b84befe3cdc03332
Tom Haywood <tom.haywood@myob.com>
2021-02-23 11:38:54 +1100

    created the project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (var i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                
+}
+        }
+    }
+}
